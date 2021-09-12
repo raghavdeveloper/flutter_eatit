630f1313077adcbaac1681f0e5c17031bcbf4c40
raghavdeveloper <raghav.developer1221@gmail.com>
2021-09-12 20:07:05 +0530

    Flutter Eatit | #18

--- lib/screens/food_details.dart
+++ lib/screens/food_details.dart
@@ -2,17 +2,28 @@ import 'package:auto_animated/auto_animated.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_eatit/const/const.dart';
+import 'package:flutter_eatit/model/size_model.dart';
 import 'package:flutter_eatit/state/category_state.dart';
+import 'package:flutter_eatit/state/food_detail_state.dart';
 import 'package:flutter_eatit/state/food_list_state.dart';
+import 'package:flutter_eatit/strings/food_detail_strings.dart';
 import 'package:flutter_eatit/strings/food_list_string.dart';
 import 'package:flutter_eatit/utils/utils.dart';
 import 'package:flutter_eatit/widgets/common/common_widgets.dart';
+import 'package:flutter_eatit/widgets/food_detail/food_detail_description_widget.dart';
+import 'package:flutter_eatit/widgets/food_detail/food_detail_image_widget.dart';
+import 'package:flutter_eatit/widgets/food_detail/food_detail_name_widget.dart';
+import 'package:flutter_eatit/widgets/food_detail/food_detail_size_widget.dart';
+import 'package:flutter_elegant_number_button/flutter_elegant_number_button.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class FoodDetailScreen extends StatelessWidget {
   final CategoryStateController categoryStateController = Get.find();
   final FoodListStateController foodListStateController = Get.find();
+  final FoodDetailStateController foodDetailStateController =
+      Get.put(FoodDetailStateController());
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -28,27 +39,60 @@ class FoodDetailScreen extends StatelessWidget {
               iconTheme: IconThemeData(color: Colors.black),
               foregroundColor: Colors.black,
               bottom: PreferredSize(
-                  preferredSize: Size.square(foodDetailsImageAreaSize(context)),
-                  child: Column(children: [
-                    Container(
-                      width: double.infinity, //max width
-                      height: foodDetailsImageAreaSize(context) * 0.9,
-                      child: CachedNetworkImage(
-                          imageUrl:
-                              foodListStateController.selectedFood.value.image,
-                          fit: BoxFit.cover,
-                          errorWidget: (context, url, err) => Center(
-                                child: Icon(Icons.image),
-                              ),
-                          progressIndicatorBuilder:
-                              (context, url, downloadProgress) => Center(
-                                    child: CircularProgressIndicator(),
-                                  )),
-                    )
-                  ])))
+                preferredSize: Size.square(foodDetailsImageAreaSize(context)),
+                child: FoodDetailImageWidget(
+                    foodListStateController: foodListStateController)
+              ))
         ];
       },
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Container(
+            margin: const EdgeInsets.only(top: 10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  FoodDetailNameWidget(
+                      foodDetailStateController: foodDetailStateController,
+                      foodListStateController: foodListStateController),
+                  FoodDetailDescriptionWidget(
+                      foodListStateController: foodListStateController),
+                  FoodDetailSizeWidget(
+                      foodListStateController: foodListStateController,
+                      foodDetailStateController: foodDetailStateController),
+                  Card(
+                      elevation: 12,
+                      child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Obx(() => ExpansionTile(
+                                        title: Text(addonText,
+                                            style: GoogleFonts.jetBrainsMono(
+                                                color: Colors.blueGrey,
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w900)),
+                                        children: [
+                                          Wrap(
+                                            children: foodListStateController
+                                                .selectedFood.value.addon
+                                                .map((e) => Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(8),
+                                                    child: ChoiceChip(
+                                                        label: Text(e.name),
+                                                        selected: foodDetailStateController.selectedAddon.contains(e),
+                                                    selectedColor: Colors.yellow,
+                                                    onSelected: (selected) => selected ? foodDetailStateController.selectedAddon.add(e) : foodDetailStateController.selectedAddon.remove(e))))
+                                                .toList(),
+                                          )
+                                        ]))
+                              ])))
+                ])),
+      ),
     )));
   }
 }
--- lib/screens/food_list.dart
+++ lib/screens/food_list.dart
@@ -43,92 +43,106 @@ class FoodListScreen extends StatelessWidget {
                             .selectedCategory.value.foods[index];
                     Get.to(() => FoodDetailScreen());
                   },
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height / 6 * 2,
-                    child: Card(
-                        semanticContainer: true,
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        child: Stack(
-                          fit: StackFit.expand,
-                          children: [
-                            CachedNetworkImage(
-                                imageUrl: categoryStateController
-                                    .selectedCategory.value.foods[index].image,
-                                fit: BoxFit.cover,
-                                errorWidget: (context, url, err) => Center(
-                                      child: Icon(Icons.image),
-                                    ),
-                                progressIndicatorBuilder:
-                                    (context, url, downloadProgress) => Center(
+                  child: Hero(
+                      tag: categoryStateController
+                          .selectedCategory.value.foods[index].name,
+                      child: SizedBox(
+                        height: MediaQuery.of(context).size.height / 6 * 2,
+                        child: Card(
+                            semanticContainer: true,
+                            clipBehavior: Clip.antiAliasWithSaveLayer,
+                            child: Stack(
+                              fit: StackFit.expand,
+                              children: [
+                                CachedNetworkImage(
+                                    imageUrl: categoryStateController
+                                        .selectedCategory
+                                        .value
+                                        .foods[index]
+                                        .image,
+                                    fit: BoxFit.cover,
+                                    errorWidget: (context, url, err) => Center(
+                                          child: Icon(Icons.image),
+                                        ),
+                                    progressIndicatorBuilder: (context, url,
+                                            downloadProgress) =>
+                                        Center(
                                           child: CircularProgressIndicator(),
                                         )),
-                            Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Container(
-                                    color: Color(COLOR_OVERLAY),
-                                    child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                top: 20, bottom: 20),
-                                            child: Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment.center,
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.center,
-                                                children: [
-                                                  Column(
-                                                      mainAxisSize:
-                                                          MainAxisSize.min,
-                                                      children: [
-                                                        Text(
-                                                            '${categoryStateController.selectedCategory.value.foods[index].name}',
-                                                            textAlign: TextAlign
-                                                                .center,
-                                                            style: GoogleFonts
-                                                                .jetBrainsMono(
+                                Align(
+                                    alignment: Alignment.bottomCenter,
+                                    child: Container(
+                                        color: Color(COLOR_OVERLAY),
+                                        child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    top: 20, bottom: 20),
+                                                child: Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .center,
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .center,
+                                                    children: [
+                                                      Column(
+                                                          mainAxisSize:
+                                                              MainAxisSize.min,
+                                                          children: [
+                                                            Text(
+                                                                '${categoryStateController.selectedCategory.value.foods[index].name}',
+                                                                textAlign:
+                                                                    TextAlign
+                                                                        .center,
+                                                                style: GoogleFonts
+                                                                    .jetBrainsMono(
+                                                                        color: Colors
+                                                                            .white,
+                                                                        fontSize:
+                                                                            18)),
+                                                            Text(
+                                                                '$priceText: \$${categoryStateController.selectedCategory.value.foods[index].price}',
+                                                                textAlign:
+                                                                    TextAlign
+                                                                        .center,
+                                                                style: GoogleFonts
+                                                                    .jetBrainsMono(
+                                                                        color: Colors
+                                                                            .white,
+                                                                        fontSize:
+                                                                            18)),
+                                                            Row(children: [
+                                                              IconButton(
+                                                                onPressed:
+                                                                    () {},
+                                                                icon: Icon(
+                                                                    Icons
+                                                                        .favorite_border,
                                                                     color: Colors
-                                                                        .white,
-                                                                    fontSize:
-                                                                        18)),
-                                                        Text(
-                                                            '$priceText: \$${categoryStateController.selectedCategory.value.foods[index].price}',
-                                                            textAlign: TextAlign
-                                                                .center,
-                                                            style: GoogleFonts
-                                                                .jetBrainsMono(
+                                                                        .white),
+                                                              ),
+                                                              SizedBox(
+                                                                  width: 50),
+                                                              IconButton(
+                                                                onPressed:
+                                                                    () {},
+                                                                icon: Icon(
+                                                                    Icons
+                                                                        .add_shopping_cart,
                                                                     color: Colors
-                                                                        .white,
-                                                                    fontSize:
-                                                                        18)),
-                                                        Row(children: [
-                                                          IconButton(
-                                                            onPressed: () {},
-                                                            icon: Icon(
-                                                                Icons
-                                                                    .favorite_border,
-                                                                color: Colors
-                                                                    .white),
-                                                          ),
-                                                          SizedBox(width: 50),
-                                                          IconButton(
-                                                            onPressed: () {},
-                                                            icon: Icon(
-                                                                Icons
-                                                                    .add_shopping_cart,
-                                                                color: Colors
-                                                                    .white),
-                                                          ),
-                                                        ])
-                                                      ])
-                                                ]),
-                                          )
-                                        ])))
-                          ],
-                        )),
-                  ),
+                                                                        .white),
+                                                              ),
+                                                            ])
+                                                          ])
+                                                    ]),
+                                              )
+                                            ])))
+                              ],
+                            )),
+                      )),
                 )),
           ))
         ]));
